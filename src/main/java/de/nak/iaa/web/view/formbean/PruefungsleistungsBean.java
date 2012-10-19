30f0767626620612df93c32889b4e33454374829
chbiel <christopher.biel89@gmail.com>
2012-10-19 19:27:19 +0200

    Pruefungsleistungsbean vorlage angepasst

--- src/main/java/de/nak/iaa/web/view/formbean/PruefungsleistungsBean.java
+++ src/main/java/de/nak/iaa/web/view/formbean/PruefungsleistungsBean.java
@@ -3,7 +3,34 @@ package de.nak.iaa.web.view.formbean;
 public class PruefungsleistungsBean {
 
 	private String examId;
+	private String matrikelNummer;
+	private String name;
+	private String alteNoten;
 	private String note;
+
+	public String getMatrikelNummer() {
+		return matrikelNummer;
+	}
+
+	public void setMatrikelNummer(String matrikelNummer) {
+		this.matrikelNummer = matrikelNummer;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAlteNoten() {
+		return alteNoten;
+	}
+
+	public void setAlteNoten(String alteNoten) {
+		this.alteNoten = alteNoten;
+	}
 
 	public String getExamId() {
 		return examId;
