e9633bbaf9ee6db8fea69408478c199e801e6000
Ronny Bräunlich <r.braeunlich@gmail.com>
2012-10-21 10:07:37 +0200

    -Init sql aufgesetzt

---
+++ init.sql
@@ -0,0 +1,14 @@
+INSERT INTO MANIPEL(ID, JAHRGANG, STUDIENRICHTUNG) VALUES
+(1, 2009, 'WInf'),
+(2, 2010, 'WInf'),
+(3, 2009, 'BWL'),
+(4, 2010, 'BWL'),
+(5, 2009, 'WIng'),
+(6, 2010, 'WIng');
+INSERT INTO PRUEFUNGSFACH(BESCHREIBUNG, TITEL, MANIPEL_ID) VALUES
+('Mathematik für die 2009er Informatiker', 'Mathematik', 1),
+('Mathematik für die 2010er Informatiker', 'Mathematik', 2),
+('Buchen, buchen, buchen', 'Rechnungswesen', 3),
+('Buchen, buchen, buchen', 'Rechnungswesen', 4),
+('Mechanik usw. 2009', 'Technische Mechanik', 5),
+('Mechanik usw. 2010', 'Technische Mechanik', 6);
