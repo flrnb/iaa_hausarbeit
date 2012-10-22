93fa7b9ba892eca7dc62b5593140e0a0d1340f0e
flrnb <fl.borchert@gmail.com>
2012-10-22 15:11:25 +0200

    neue Methoden in PruefungServiceImpl leer implementiert

--- src/main/java/de/nak/iaa/server/business/impl/PruefungServiceImpl.java
+++ src/main/java/de/nak/iaa/server/business/impl/PruefungServiceImpl.java
@@ -2,9 +2,11 @@ package de.nak.iaa.server.business.impl;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
@@ -35,13 +37,12 @@ public class PruefungServiceImpl implements PruefungService {
 	@Override
 	public List<Pruefungsfach> getAllPruefungsfaecher(final Manipel manipel) {
 		// pruefungsfachDAO.findByManipel(manipel);
-		return ImmutableList.copyOf(Iterables.filter(
-				pruefungsfachDAO.findAll(), new Predicate<Pruefungsfach>() {
-					@Override
-					public boolean apply(Pruefungsfach fach) {
-						return manipel.equals(fach.getManipel());
-					}
-				}));
+		return ImmutableList.copyOf(Iterables.filter(pruefungsfachDAO.findAll(), new Predicate<Pruefungsfach>() {
+			@Override
+			public boolean apply(Pruefungsfach fach) {
+				return manipel.equals(fach.getManipel());
+			}
+		}));
 	}
 
 	public void setPruefungsfachDAO(PruefungsfachDAO pruefungsfachDAO) {
@@ -54,21 +55,12 @@ public class PruefungServiceImpl implements PruefungService {
 	}
 
 	@Override
-	public void addPruefungsleistung(Pruefung pruefung, Date datum,
-			Student student, Note note) {
+	public void addPruefungsleistung(Pruefung pruefung, Date datum, Student student, Note note) {
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-	public List<Pruefungsleistung> getAllPruefungsleistungen(
-			Pruefungsfach fach, Student student) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Student> getAllErgaenzungsPruefungsStudenten(Manipel manipel,
-			Pruefungsfach fach) {
+	public List<Pruefungsleistung> getAllPruefungsleistungen(Pruefungsfach fach, Student student) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -79,14 +71,43 @@ public class PruefungServiceImpl implements PruefungService {
 	}
 
 	@Override
-	public List<Pruefung> getAllPruefung(Manipel manipel,
-			Pruefungsfach pruefungsfach) {
+	public Pruefung getPruefungById(Long id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Pruefung getPruefungById(Long id) {
+	public List<Pruefung> getAllPruefungen(Pruefungsfach pruefungsfach) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addPruefung(Pruefungsfach fach, Date datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isPruefungsleistungEditable(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Map<Student, Optional<Pruefungsleistung>> getAllStudentenForPruefung(Pruefung pruefung) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Note> getAktuelleNote(Student student, Pruefungsfach fach) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Student, Date> getAllErgaenzungsPruefungsStudenten(Manipel manipel, Pruefungsfach fach) {
 		// TODO Auto-generated method stub
 		return null;
 	}
