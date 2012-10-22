1da35b83c6d66ca6874800beb8b6aa60cd821697
flrnb <fl.borchert@gmail.com>
2012-10-22 18:06:14 +0200

    Tests für Prüfungsservice erweitert

--- src/test/java/de/nak/iaa/server/business/impl/PruefungServiceImplTest.java
+++ src/test/java/de/nak/iaa/server/business/impl/PruefungServiceImplTest.java
@@ -1,9 +1,10 @@
 package de.nak.iaa.server.business.impl;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 import static org.junit.matchers.JUnitMatchers.hasItems;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -40,13 +41,27 @@ public class PruefungServiceImplTest {
 
 	private Manipel man2;
 
+	private Manipel man3;
+
 	private Student student;
+
+	private static final Date TODAY = new Date();
+
+	private static Date TOMORROW;
+
+	public static void setUpBeforeClass() {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(TODAY);
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+		TOMORROW = cal.getTime();
+	}
 
 	@Before
 	public void setUp() {
 		service = new PruefungServiceImpl();
 		man1 = new Manipel(2009, Studienrichtung.WInf);
 		man2 = new Manipel(2010, Studienrichtung.WInf);
+		man3 = new Manipel(2010, Studienrichtung.WIng);
 		fach1 = new Pruefungsfach("Fach1", "Beschreibung1", man1);
 		fach2 = new Pruefungsfach("Fach2", "Beschreibung2", man1);
 		fach3 = new Pruefungsfach("Fach3", "Beschreibung3", man2);
@@ -60,24 +75,32 @@ public class PruefungServiceImplTest {
 	public void testGetAllPruefungsfaecher() {
 		assertThat(service.getAllPruefungsfaecher(man1), hasItems(fach1, fach2));
 		assertThat(service.getAllPruefungsfaecher(man2), hasItems(fach3));
+		assertThat(service.getAllPruefungsfaecher(man3).size(), is(0));
 	}
 
 	@Test
 	public void testIsPruefungsleistungEditable() {
-		Pruefung pruefung1 = new Pruefung();
-		service.addPruefungsleistung(pruefung1, new Date(), student, Note.Fuenf);
-		service.getAllPruefungsleistungen(, student)
+		Pruefung pruefung1 = new Pruefung(TODAY, fach1);
+		Pruefungsleistung leistung = service.addPruefungsleistung(pruefung1, student, Note.Fuenf);
+		assertTrue(service.isPruefungsleistungEditable(leistung.getId()));
 	}
 
 	@Test
 	public void testIsPruefungsleistungNotEditable() {
-
+		Pruefung pruefung1 = new Pruefung(TODAY, fach1);
+		service.addPruefungsleistung(pruefung1, student, Note.Fuenf);
+		Pruefungsleistung leistung1 = service.getAllPruefungsleistungen(fach1, student).get(0);
+		Pruefung pruefung2 = new Pruefung(TODAY, fach1);
+		service.addPruefungsleistung(pruefung2, student, Note.Fuenf);
+		Pruefungsleistung leistung2 = service.getAllPruefungsleistungen(fach1, student).get(0);
+		assertFalse(service.isPruefungsleistungEditable(leistung1.getId()));
+		assertTrue(service.isPruefungsleistungEditable(leistung2.getId()));
 	}
 
 	@Test
 	public void testUpdatePruefungsleistung() {
-		Pruefung pruefung = new Pruefung(new Date(), fach1);
-		service.addPruefungsleistung(pruefung, new Date(), student, Note.EinsDrei);
+		Pruefung pruefung = new Pruefung(TODAY, fach1);
+		service.addPruefungsleistung(pruefung, student, Note.EinsDrei);
 		List<Pruefungsleistung> allPruefungsleistungen = service.getAllPruefungsleistungen(fach1, student);
 		assertThat(allPruefungsleistungen.size(), is(1));
 		Pruefungsleistung leistung = allPruefungsleistungen.get(0);
@@ -85,13 +108,52 @@ public class PruefungServiceImplTest {
 		assertThat(leistung.getNote(), is(Note.EinsSieben));
 	}
 
-	@Test
-	public void testUpdatePruefungsleistungNotEditable() {
+	@Test(expected = IllegalStateException.class)
+	public void testUpdatePruefungsleistungNotEditableIllegalState() {
 		Pruefung pruefung1 = new Pruefung();
-		service.addPruefungsleistung(pruefung1, new Date(), student, Note.Fuenf);
+		service.addPruefungsleistung(pruefung1, student, Note.Fuenf);
+		Pruefungsleistung leistung1 = service.getAllPruefungsleistungen(fach1, student).get(0);
 		Pruefung pruefung2 = new Pruefung();
+		service.addPruefungsleistung(pruefung2, student, Note.ZweiDrei);
+		service.updatePruefungsleistung(leistung1.getId(), Note.Vier);
+	}
 
-		service.addPruefungsleistung(pruefung2, new Date(), student, Note.ZweiDrei);
+	@Test
+	public void testGetPruefungsfachById() {
+		assertThat(service.getPruefungsfachById(fach2.getId()), is(equalTo(fach2)));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetPruefungsfachByIdNotPresent() {
+		service.getPruefungsfachById(1000L);
+	}
+
+	@Test
+	public void testGetAllPruefungen() {
+		assertThat(service.getAllPruefungen(fach1).size(), is(0));
+		service.addPruefung(fach1, TODAY);
+		service.addPruefung(fach1, TOMORROW);
+		service.addPruefung(fach2, TODAY);
+		service.getAllPruefungen(fach1);
+		assertThat(service.getAllPruefungen(fach1).size(), is(2));
+	}
+
+	@Test
+	public void testGetPruefungById() {
+		service.addPruefung(fach1, TODAY);
+		Pruefung pruefung = service.getAllPruefungen(fach1).get(0);
+		assertThat(service.getPruefungById(pruefung.getId()), is(equalTo(pruefung)));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetPruefungByIdNotPresent() {
+		service.getPruefungById(1000L);
+	}
+
+	@Test
+	public void testGetAllPruefungsleistungen() {
+		// service.addPruefungsleistung(pruefung, student, note)
+		service.getAllPruefungsleistungen(fach1, student);
 	}
 
 }
