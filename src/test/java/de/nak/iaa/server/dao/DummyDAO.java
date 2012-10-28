6ca49e24f22837278de8d8a1054d1c14abec0776
flrnb <fl.borchert@gmail.com>
2012-10-28 23:45:16 +0100

    - Beispiel-Testklassen entfernt

--- src/test/java/de/nak/iaa/server/dao/DummyDAO.java
+++
@@ -1,9 +0,0 @@
-package de.nak.iaa.server.dao;
-
-import de.nak.iaa.server.entity.DummyEntity;
-
-/**
- * Plain DAO which provides only {@link de.nak.iaa.server.dao.impl.GenericHibernateDAOImpl} methods
- */
-public interface DummyDAO extends GenericDAO<DummyEntity, Long> {
-}
