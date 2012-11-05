fc6a15a29e97914cd2adb1b533ebd14da578ce52
chbiel <christopher.biel89@gmail.com>
2012-11-05 19:01:46 +0100

    kommentar

--- src/main/java/de/nak/iaa/server/fachwert/Note.java
+++ src/main/java/de/nak/iaa/server/fachwert/Note.java
@@ -4,9 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum Note {
-	Eins(1.0), EinsDrei(1.3), EinsSieben(1.7), Zwei(2.0), ZweiDrei(2.3), ZweiSieben(
-			2.7), Drei(3.0), DreiDrei(3.3), DreiSieben(3.7), Vier(4.0), Fuenf(
-			5.0), Sechs(6.0);
+	Eins(1.0), EinsDrei(1.3), EinsSieben(1.7), Zwei(2.0), ZweiDrei(2.3), ZweiSieben(2.7), Drei(3.0), DreiDrei(3.3), DreiSieben(
+			3.7), Vier(4.0), Fuenf(5.0), Sechs(6.0);
 
 	private Double note;
 	private static final Map<Double, Note> lookup = new HashMap<Double, Note>();
@@ -24,6 +23,13 @@ public enum Note {
 		this.note = note;
 	}
 
+	/**
+	 * Hole eine Note zu einem String
+	 * 
+	 * @author Christopher Biel <christopher.biel89@gmail.com>
+	 * @param note
+	 * @return
+	 */
 	public static Note getNote(String note) {
 		if (note.matches("[123456][.][037]") || note.contains("."))
 			return lookup.get(Double.valueOf(note));
@@ -31,6 +37,13 @@ public enum Note {
 			return lookup.get(Double.valueOf(note + ".0"));
 	}
 
+	/**
+	 * Prüfe of eine Note valide ist
+	 * 
+	 * @author Christopher Biel <christopher.biel89@gmail.com>
+	 * @param note
+	 * @return
+	 */
 	public static boolean isValid(String note) {
 		return (Note.getNote(note) == null) ? false : true;
 	}
